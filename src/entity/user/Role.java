993a4019af958fb085e3266edaef690443e16586
VanilaMint <minhhh36@gmail.com>
2024-06-14 23:06:22 +0700

    added crud user controller/entity

---
+++ src/entity/user/Role.java
@@ -0,0 +1,31 @@
+package entity.user;
+
+public class Role {
+    private int id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(int id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    // Getters and setters
+    public int getRoleId() {
+        return id;
+    }
+
+    public void setRoleId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
